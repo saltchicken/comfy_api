32eee64f817eb536dfce64bcf52bf873e707768f
saltchicken <john.eicher89@gmail.com>
2025-02-11 20:17:50 -0800

    Add generators to example

--- src/comfy_api/example.py
+++ src/comfy_api/example.py
@@ -2,7 +2,7 @@ from comfy_api import ComfyClient
 
 comfy = ComfyClient("10.0.0.3:8188")
 comfy.length = 72
-comfy.prompt = "A sample prompt"
+comfy.prompt = "fractal"
 comfy.boomerang = True
 
 def generator(initial, end, increment):
@@ -14,8 +14,13 @@ def generator(initial, end, increment):
         value = round(value + increment, 2)
 
 x_gen = generator(0.0, 1.0, 0.01)
+y_gen = generator(0.5, 1.0, 0.01)
+z_gen = generator(0.8, 1.0, -0.02)
+
+# for i in range(100):
+#     print(x_gen.__next__())
 
 for i in range(100):
-    print(x_gen.__next__())
-
-# c mfy.view_video(videos)
+    comfy.lora = [f"fractalanimationfrost={next(x_gen)}]
+    videos = comfy.run_workflow()
+# comfy.view_video(videos)
